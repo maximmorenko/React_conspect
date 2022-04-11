373536bae7af1c8b04f22fc97303b8b94528fbb5
Maksym Morenko <morenko.maxim@gmail.com>
2022-04-11 06:54:12 +0300

    виды создания компонентов

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
-// реакт импортируем в каждый файл где создаем проект
+import React from 'react'; // реакт импортируем в каждый файл где создаем проект
+
+import FuncComponent from './components/FuncComponent';
+import ClassComponent from './components/ClassComponent';
+
 
 
 function App() {
   return (
     <div className="App">
-      
+      <FuncComponent />
+      <ClassComponent />
     </div>
   );
 }
