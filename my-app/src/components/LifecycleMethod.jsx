272efe31fb299675ffc2d0dacc8bee91a73ddd9c
Maksym Morenko <morenko.maxim@gmail.com>
2022-05-02 04:37:14 +0300

    componentDidMount() и слушатель события

--- my-app/src/components/LifecycleMethod.jsx
+++ my-app/src/components/LifecycleMethod.jsx
@@ -34,3 +34,110 @@
 //     return <div />
 //   }
 // };
+
+// Більшість веб-розробників в якийсь момент мають викликати кінцеву точку API, 
+// щоб отримати дані. Якщо ви працюєте у React, потрібно розуміти як виконати цю дію.
+
+// Найкращим рішенням буде розмістити виклик API, 
+// або будь-які інші виклики направлені до сервера у методі 
+// життєвого циклу componentDidMount(). Цей метод викликають після того, 
+// як компонент встановлено в DOM. 
+// Будь-які виклики setState() активують ререндер вашого компонента. 
+// Коли ви викликаєте API у цьому методі й встановлюєте стан 
+// відповідно до вихідних API даних, цей метод автоматично запустить оновлення.
+
+// В componentDidMount() є імітація виклику API. 
+// Через 1 секунди він симулює виклик до сервера для отримання даних. 
+// У цьому прикладі йде запит на кількість поточних активних користувачів сайту. 
+// У рендер методі покажіть значення activeUsers у h1 після 
+// тексту Active Users:. Подивіться, що відбувається при попередньому 
+// перегляді та спробуйте змінити таймаут, щоб побачити різні ефекти.
+
+// class MyComponent extends React.Component {
+//     constructor(props) {
+//       super(props);
+//       this.state = {
+//         activeUsers: null,
+//         activeUser: 673
+//       };
+//     }
+//     componentDidMount() {
+//       setTimeout(() => {
+//         this.setState({
+//           activeUsers: 1273
+          
+//         });
+//       }, 1000);
+//     }
+//     render() {
+//       return (
+//         <div>
+//           {/* Змініть код під цим рядком */}
+//           <h1>Active Users: {this.state.activeUsers}</h1>
+//           {/* Змініть код над цим рядком */}
+//         </div>
+//       );
+//     }
+//   }
+
+  //Active Users: 1273
+
+  //////////////////////////////////////////////////////////////
+
+//   В этой задаче вам нужно изменить два метода жизненного цикла: 
+//   componentDidMount и componentWillUnmount. 
+//   Вы используете componentDidMount, когда хотите что-то настроить, 
+//   в вашем случае прослушиватель событий. 
+//   Вы используете componentWillUnmount, когда вам нужно что-то очистить, ваш прослушиватель событий.
+
+// Вы добавите прослушиватель событий в метод componentDidMount, 
+// который будет прослушивать событие «keydown». 
+// Используйте document.addEventListener (событие, функция, необязательный (useCapture)). 
+// Затем удалите этот же прослушиватель событий, 
+// передав те же аргументы в document.removeEventListener(event, function, optional(useCapture))
+//  в методе componentWillUnmount.
+
+// Примечание: document.addEventListener и document.removeEventListener будут 
+// принимать строку в кавычках для своего события, и при передаче функции
+//  вы будете ссылаться на функцию handleKeyPress() как this.handleKeyPress. 
+//  Если вы вызовете функцию как this.handleKeyPress(), прослушиватель 
+//  событий сначала оценит функцию и вернет значение undefined.
+
+// class MyComponent extends React.Component {
+//     constructor(props) {
+//       super(props);
+//       this.state = {
+//         message: ''
+//       };
+//       this.handleEnter = this.handleEnter.bind(this);
+//       this.handleKeyPress = this.handleKeyPress.bind(this);
+//     }
+//     // Змініть код під цим рядком
+//     componentDidMount() {
+//       document.addEventListener('keydown', this.handleKeyPress)
+  
+//     }
+//     componentWillUnmount() {
+//       document.removeEventListener('keydown', this.handleKeyPress)
+  
+//     }
+//     // Змініть код над цим рядком
+//     handleEnter() {
+//       this.setState((state) => ({
+//         message: state.message + 'You pressed the enter key! '
+//       }));
+//     }
+//     handleKeyPress(event) {
+//       if (event.keyCode === 13) {
+        
+//         this.handleEnter();
+//       }
+//     }
+//     render() {
+//       return (
+//         <div>
+//           <h1>{this.state.message}</h1>
+//         </div>
+//       );
+//     }
+//   };
